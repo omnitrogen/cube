f3ef90256f41b324c8dd2184281531b5533693dc
omnitrogen <felixgif@gmail.com>
2017-10-19 21:05:44 +0000

    modify rgb values for the yellow colors

--- rubiks.py
+++ rubiks.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from cv2 import *
 
-im = cv2.imread('yellowrsz.png')
+im = cv2.imread('bright.png')
 im = cv2.bilateralFilter(im,9,75,75)
 im = cv2.fastNlMeansDenoisingColored(im,None,10,10,7,21)
 cv2.imshow("Show",im)
@@ -40,17 +40,16 @@ cv2.imshow("YCrCb", ycrcb_img)
 cv2.imwrite("ycrcb.jpg", ycrcb_img)
 
 
-COLOR_MIN = np.array([20, 100, 100],np.uint8)       # HSV color code lower and upper bounds
-COLOR_MAX = np.array([30, 255, 255],np.uint8)       # color yellow 
+COLOR_MIN = np.array([17, 99, 140],np.uint8)       # HSV color code lower and upper bounds
+COLOR_MAX = np.array([68, 255, 255],np.uint8)       # color yellow 
 
 frame_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)     # Thresholding image
-imgray = frame_threshed
 ret,thresh = cv2.threshold(frame_threshed,127,255,0)
 _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-print(type(contours))
-print(contours)
-for cnt in contours:
-    x,y,w,h = cv2.boundingRect(cnt)
+#print(type(contours))
+#print(contours)
+for contour in contours:
+    x,y,w,h = cv2.boundingRect(contour)
     print(x,y)
     cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
 cv2.imshow("Show",im)
