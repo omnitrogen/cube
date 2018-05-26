134b52fb6fba2978e22197b7b2987462b48ed824
Félix Defrance <felixgif@gmail.com>
2018-05-27 00:33:15 +0200

    add use of enable pins on the arduino

--- ard/ard.ino
+++ ard/ard.ino
@@ -4,15 +4,17 @@ const int spr = 200 * 32;
 void setup() {
     Serial.begin(9600);
 
-    for (int i=2 ; i<12 ; i++){
+    for (int i=2; i<12; i++){
         pinMode(i, OUTPUT);
         digitalWrite(i,LOW);
     }
 
-    // maybe add enable PINS ?
+    for (int i=14; i<19; i++){
+        pinMode(i, OUTPUT);
+        digitalWrite(i,HIGH);
+    }
 
     delay(1000);
-    // Serial.print("init");
 }
 
 void loop() {
@@ -25,84 +27,79 @@ void loop() {
             switch (inData[i]) {
 
             case 'a':
-                turn(0, 3, 2, 0.25);
+                turn(0, 3, 2, 0.25, 14);
                 break;
 
             case 'b':
-                turn(0, 3, 2, 0.5);
+                turn(0, 3, 2, 0.5, 14);
                 break;
 
             case 'c':
-                turn(1, 3, 2, 0.25);
+                turn(1, 3, 2, 0.25, 14);
                 break;
 
             case 'd':
-                turn(0, 5, 4, 0.25);
+                turn(0, 5, 4, 0.25, 15);
                 break;
 
             case 'e':
-                turn(0, 5, 4, 0.5);
+                turn(0, 5, 4, 0.5, 15);
                 break;
 
             case 'f':
-                turn(1, 5, 4, 0.25);
+                turn(1, 5, 4, 0.25, 15);
                 break;
 
             case 'g':
-                turn(0, 7, 6, 0.25);
+                turn(0, 7, 6, 0.25, 16);
                 break;
 
             case 'h':
-                turn(0, 7, 6, 0.5);
+                turn(0, 7, 6, 0.5, 16);
                 break;
 
             case 'i':
-                turn(1, 7, 6, 0.25);
+                turn(1, 7, 6, 0.25, 16);
                 break;
 
             case 'j':
-                turn(0, 9, 8, 0.25);
+                turn(0, 9, 8, 0.25, 17);
                 break;
 
             case 'k':
-                turn(0, 9, 8, 0.5);
+                turn(0, 9, 8, 0.5, 17);
                 break;
 
             case 'l':
-                turn(1, 9, 8, 0.25);
+                turn(1, 9, 8, 0.25, 17);
                 break;
 
             case 'm':
-                turn(0, 11, 10, 0.25);
+                turn(0, 11, 10, 0.25, 18);
                 break;
 
             case 'n':
-                turn(0, 11, 10, 0.5);
+                turn(0, 11, 10, 0.5, 18);
                 break;
 
             case 'o':
-                turn(1, 11, 10, 0.25);
-                break;
-
-            case 'z':
-                Serial.write("DN");
-                delay(1000);
+                turn(1, 11, 10, 0.25, 18);
                 break;
 
             }
-            // Serial.write(inData[i]);
         }
     }
 }
 
-void turn(int sens, int pinPas, int pinSens, float rot) {
+void turn(int sens, int pinPas, int pinSens, float rot, int enable) {
 
     digitalWrite(pinSens, sens);
-
+    digitalWrite(enable, LOW);
     for(int k = 0; k < spr * rot; k++){
         digitalWrite(pinPas, HIGH);
-        delayMicroseconds(50); // ?
         digitalWrite(pinPas, LOW);
         delayMicroseconds(50);
     }
+    digitalWrite(enable, HIGH);
+    delayMicroseconds(1000);
 }
